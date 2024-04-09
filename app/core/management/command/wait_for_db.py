9538214fba3945419d59c8083ccc25ef06509567
michaelmbb <60005676+michaelmbb@users.noreply.github.com>
2024-04-09 04:39:54 +0000

    update

---
+++ app/core/management/command/wait_for_db.py
@@ -0,0 +1,13 @@
+"""
+Djano command to wait for the databse to be available
+"""
+from typing import Any
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+    
+    def handle(self, *args, **options):
+        pass
+    
